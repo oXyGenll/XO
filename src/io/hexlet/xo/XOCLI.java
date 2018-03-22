f66b8c71069c90a1ba4427d9ae4ff32d4c633e02
Aleksandr Panteleev <alex_kc@mail.ru>
2018-03-22 20:52:40 +0300

    test

--- src/io/hexlet/xo/XOCLI.java
+++ src/io/hexlet/xo/XOCLI.java
@@ -9,7 +9,6 @@ import io.hexlet.xo.view.ConsoleView;
 
 public class XOCLI {
 
-
     public static void main(final String[] args) {
         final String name1 = "Player1";
         final String name2 = "Player2";
